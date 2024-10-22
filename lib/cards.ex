5caabca7794995b0ea93097e52d4243fd60e3698
Practice <ranapratik47904@gmail.com>
2024-10-22 09:20:32 +0530

    documentation and pipe operator

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,6 +6,9 @@ defmodule Cards do
     "hi there"
   end
 
+  @doc"""
+  it is used to create deck
+  """
   def create_deck do
     values=["ace","two","three"]
     suits=["spades","clubs","Hearts"]
@@ -24,8 +27,40 @@ defmodule Cards do
     Enum.member?(deck,card)
   end
 
+  @doc """
+   Divides a deck into a hand and reminder of the deck.The 'hand_size' argument indicates how
+   many cards should be in the hand
+
+  ## Examples
+      iex> deck=Cards.create_deck
+      iex> {hand,deck}=Cards.deal(deck,1)
+      iex> hand
+      ["ace space"]
+
+
+  """
   def deal(deck,hand_size) do
     Enum.split(deck,hand_size)
+  end
+
+  def save(deck,filename) do
+    binary=:erlang.term_to_binary(deck)
+    File.write(filename,binary)
+  end
+
+  def load(filename) do
+    {status,binary}=File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error->"That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
   @doc """
   Hello world.
