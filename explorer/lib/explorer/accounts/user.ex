577f1c2b1f90087642e5cbc2a3b005934518b95f
Practice <ranapratik47904@gmail.com>
2024-10-25 09:45:59 +0530

    users and post and user association

---
+++ explorer/lib/explorer/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule Explorer.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    has_many :posts,Explorer.Posts.Post
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
+    |> unique_constraint(:email)
+  end
+end
---
+++ explorer/lib/explorer_web/controllers/user_json.ex
@@ -0,0 +1,34 @@
+defmodule ExplorerWeb.UserJSON do
+  alias Explorer.Accounts.User
+  alias Explorer.Posts.Post
+
+  @doc """
+  Renders a list of users.
+  """
+  def index(%{users: users}) do
+    %{data: for(user <- users, do: data(user))}
+  end
+
+  @doc """
+  Renders a single user.
+  """
+  def show(%{user: user}) do
+    %{data: data(user)}
+  end
+
+  def data(%User{} = user) do
+    %{
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      posts: for(post <- user.posts,do: post_data(post))
+    }
+  end
+  def post_data(%Post{} = post) do
+    %{
+      id: post.id,
+      title: post.title,
+      body: post.body,
+    }
+  end
+end
